b5935afb18b140ddfd763003677e62a33eb4db79
Michael Ulm <michael.ulm@mea-it.services>
2021-05-17 07:32:20 +0200

    initial commit with just a simple demo output

---
+++ src/main/java/at/fhj/iit/App.java
@@ -0,0 +1,17 @@
+package at.fhj.iit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public void sayHello(String name){
+        System.out.println("Hello, " + name);
+    }
+}
